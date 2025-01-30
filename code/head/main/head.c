e8df70b7e47a5c2c5a90ec1ba76fae076a37769c
Xz3ra <mathieu_durand@icloud.com>
2025-01-30 11:11:37 -0500

    Including MAC address in the device name turn's out to not be useful

--- code/head/main/head.c
+++ code/head/main/head.c
@@ -21,10 +21,5 @@ void parse_received_message(struct Message *message) {
 }
 
 void app_main(void) {
-    unsigned char mac[6];
-    esp_read_mac(mac, ESP_MAC_WIFI_STA);
-    char name[50];
-    sprintf(name, "SHERPANT_MODULE_%02X:%02X:%02X:%02X:%02X:%02X", mac[0], mac[1], mac[2], mac[3], mac[4], mac[5]);
-
-    ble_main(name, parse_received_message);
+    ble_main("SHERPANT_MODULE", parse_received_message);
 }
